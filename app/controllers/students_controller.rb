a213bceda2569934d05af9770f041e108961e88d
Dan <dt.lefever@gmail.com>
2021-12-13 10:31:39 -0700

    commit tree 80d16c67dd1c6ab07e56657b2da51907d27ca8d6

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        students= Student.all
+        render json: students
+    end
+
+    def grades
+        students_grades= Student.order(grade: :desc)
+        render json: students_grades
+    end
+
+    def highest_grade
+        students_highest_grades= Student.order(grade: :desc).first
+        render json: students_highest_grades
+    end
+
+end
